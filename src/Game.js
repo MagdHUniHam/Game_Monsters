83c9a83de27f6902f02f9dac9e3580c725c028ca
MagdHUniHam <magdalena.heynicke@uni-hamburg.de>
2025-04-23 15:56:52 +0200

    Update Game.js

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,8 @@
 Candy.Game = function(game) {
-    // define needed variables for Candy.Game
     this._player = null;
     this._candyGroup = null;
     this._spawnCandyTimer = 0;
     this._fontStyle = null;
-    // define Candy variables to reuse them in Candy.item functions
     Candy._scoreText = null;
     Candy._score = 0;
     Candy._health = 0;
@@ -15,9 +13,8 @@ Candy.Game.prototype = {
     create: function() {
         // start the physics engine
         this.physics.startSystem(Phaser.Physics.ARCADE);
-        // set the global gravity
         this.physics.arcade.gravity.y = 200;
-        // display images: background, floor, and score
+        // display images
         this.add.sprite(0, 0, 'background');
         this.add.sprite(-30, Candy.GAME_HEIGHT - 160, 'floor');
         this.add.sprite(10, 5, 'score-bg');
@@ -25,9 +22,7 @@ Candy.Game.prototype = {
         this.add.button(Candy.GAME_WIDTH - 96 - 10, 5, 'button-pause', this.managePause, this);
         // create the player
         this._player = this.add.sprite(5, 760, 'monster-idle');
-        // add player animation
         this._player.animations.add('idle', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 10, true);
-        // play the animation
         this._player.animations.play('idle');
         // set font style
         this._fontStyle = { font: "40px Arial", fill: "#FFCC00", stroke: "#333", strokeThickness: 5, align: "center" };
@@ -44,139 +39,51 @@ Candy.Game.prototype = {
     },
 
     managePause: function() {
-        // pause the game
         this.game.paused = true;
-        // add proper informational text
         var pausedText = this.add.text(100, 250, "Game paused.\nTap anywhere to continue.", this._fontStyle);
-        // set event listener for the user's click/tap the screen
         this.input.onDown.add(function() {
-            // remove the pause text
             pausedText.destroy();
-            // unpause the game
             this.game.paused = false;
         }, this);
     },
 
     update: function() {
-        // update timer every frame
         this._spawnCandyTimer += this.time.elapsed;
-        // if spawn timer reach one second (1000 milliseconds)
+
         if (this._spawnCandyTimer > 1000) {
-            // reset it
             this._spawnCandyTimer = 0;
-            // and spawn new candy
             Candy.item.spawnCandy(this);
         }
 
-        // loop through all candy on the screen
         this._candyGroup.forEach(function(candy) {
-            // to rotate them accordingly
             candy.angle += candy.rotateMe;
         });
 
-        // if the health of the player drops to 0, the player dies = game over
         if (!Candy._health && !this._gameOverShown) {
             this._gameOverShown = true;
 
-            // show the game over message
+            // show game over screen
             this.add.sprite((Candy.GAME_WIDTH - 594) / 2, (Candy.GAME_HEIGHT - 271) / 2, 'game-over');
 
-            // add the button that will restart the game
+            // add the play again button
             var playButton = this.add.button(
                 Candy.GAME_WIDTH - 401 - 10,
                 Candy.GAME_HEIGHT - 143 - 10,
                 'button-start',
                 function () {
-                    this.game.paused = false; // unpause the game first
-                    this.state.start('Game'); // then restart the game
+                    this.game.paused = false; // unpause the game
+                    this.state.start('Game'); // restart the game
                 },
                 this,
                 1, 0, 2
             );
-
-            // add input listener to make sure the button is clickable
             playButton.inputEnabled = true;
             playButton.events.onInputDown.add(function() {
-                this.game.paused = false; // unpause game
-                this.state.start('Game'); // restart game
+                this.game.paused = false;
+                this.state.start('Game');
             }, this);
 
-            // pause the game (UI elements like buttons still work in Phaser)
             this.game.paused = true;
         }
-    }
-};
-
-// Main Menu state for starting the game
-Candy.MainMenu = function(game) {};
-
-Candy.MainMenu.prototype = {
-    create: function() {
-        // display images: background and title
-        this.add.sprite(0, 0, 'background');
-        this.add.sprite(-130, Candy.GAME_HEIGHT - 514, 'monster-cover');
-        this.add.sprite((Candy.GAME_WIDTH - 395) / 2, 60, 'title');
-
-        // add the start button
-        var startButton = this.add.button(Candy.GAME_WIDTH - 401 - 10, Candy.GAME_HEIGHT - 143 - 10, 'button-start', this.startGame, this, 1, 0, 2);
-
-        // Make sure the button works when clicked
-        startButton.inputEnabled = true;
-        startButton.events.onInputDown.add(this.startGame, this);
-    },
-
-    startGame: function() {
-        // Start the game state
-        this.state.start('Game');
-    }
-};
-
-// candy spawning and interaction logic remains the same
-Candy.item = {
-    spawnCandy: function(game) {
-        // calculate drop position (from 0 to game width) on the x axis
-        var dropPos = Math.floor(Math.random() * Candy.GAME_WIDTH);
-        // define the offset for every candy
-        var dropOffset = [-27, -36, -36, -38, -48];
-        // randomize candy type
-        var candyType = Math.floor(Math.random() * 5);
-        // create new candy
-        var candy = game.add.sprite(dropPos, dropOffset[candyType], 'candy');
-        // add new animation frame
-        candy.animations.add('anim', [candyType], 10, true);
-        // play the newly created animation
-        candy.animations.play('anim');
-        // enable candy body for physic engine
-        game.physics.enable(candy, Phaser.Physics.ARCADE);
-        // enable candy to be clicked/tapped
-        candy.inputEnabled = true;
-        // add event listener to click/tap
-        candy.events.onInputDown.add(this.clickCandy, this);
-        // be sure that the candy will fire an event when it goes out of the screen
-        candy.checkWorldBounds = true;
-        // reset candy when it goes out of screen
-        candy.events.onOutOfBounds.add(this.removeCandy, this);
-        // set the anchor (for rotation, position etc) to the middle of the candy
-        candy.anchor.setTo(0.5, 0.5);
-        // set the random rotation value
-        candy.rotateMe = (Math.random() * 4) - 2;
-        // add candy to the group
-        game._candyGroup.add(candy);
-    },
-
-    clickCandy: function(candy) {
-        // kill the candy when it's clicked
-        candy.kill();
-        // add points to the score
-        Candy._score += 1;
-        // update score text
-        Candy._scoreText.setText(Candy._score);
-    },
-
-    removeCandy: function(candy) {
-        // kill the candy
-        candy.kill();
-        // decrease player's health
-        Candy._health -= 10;
     }
 };
