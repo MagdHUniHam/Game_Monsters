7685331adb3e109bbc5b45bf1707527c29e31724
MagdHUniHam <magdalena.heynicke@uni-hamburg.de>
2025-04-24 12:30:31 +0200

    Update Game.js

--- src/Game.js
+++ src/Game.js
@@ -74,13 +74,8 @@ Candy.Game.prototype = {
 			this.add.sprite((Candy.GAME_WIDTH-594)/2, (Candy.GAME_HEIGHT-271)/2, 'game-over');
 			// pause the game
 			this.game.paused = true;
-			// add the button that will start the game
-			this.add.button(Candy.GAME_WIDTH-401-10, Candy.GAME_HEIGHT-143-10, 'button-start', this.startGame, this, 1, 0, 2);
-			},
-	startGame: function() {
-	// start the Game state
-	this.state.start('Game');
 		}
+	}
 };
 
 Candy.item = {
